59e4f2540fd0f6301c26f82d133a32b12b79896b
Kislenko Mikhail <5511270+Vampire2008@users.noreply.github.com>
2024-10-19 03:12:42 +0400

    Added ability to save picture between reloads + total rewrire (#7)
    * Totaly rewrited to support saving texture
    Removed garbage from repository
    
    * SetRandomGenerator if no sets
    Fixed script
    
    * Code for loading textures moved to another file
    Fixed managing default textures.
    Fixed loading LethalPainting mods
    Added dummy module if no textures loaded, but it enables saving data in Paiting item for compapability
    
    * Fixes
    
    * Removed dependency of CSync
    
    ---------
    
    Co-authored-by: Mikhail Kislenko <mikhail.kislenko@devexpress.com>

---
+++ package.ps1
@@ -0,0 +1,22 @@
+$output = "$PSScriptRoot/dist"
+$publishPath = "$PSScriptRoot/publish"
+
+if (Test-Path $output) {
+    Remove-Item $output -Recurse
+}
+
+New-Item $output -ItemType Directory
+
+dotnet publish "$PSScriptRoot/CustomPainting/CustomPainting.csproj" -o $publishPath
+
+Copy-Item "$PSScriptRoot/publish/CustomPainting.dll" -Destination $output
+Copy-Item "$PSScriptRoot/package/*" -Destination $output
+Copy-Item "$PSScriptRoot/CHANGELOG.md" -Destination $output
+
+$resultFile = "$PSScriptRoot/CustomPainting.zip"
+
+if (Test-Path $resultFile) {
+    Remove-Item $resultFile
+}
+
+Compress-Archive -Path "$output/*" -DestinationPath $resultFile
